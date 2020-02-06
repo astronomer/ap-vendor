3152eb8c3680122a4f4c2a53fbc0f1383cfc6317
Andrii Soldatenko <andrii.soldatenko@gmail.com>
2020-02-06 22:53:51 +0200

    Fix prisma crashloop (#4)
    * fix prisma crashllops via adding wait-for-db.sh
    
    * fix ci
    
    * bump versions of grafana/kibana
    
    * Bump patch version of ElasticSearch
    
    * undo wrong bump
    
    * Bump prisma
    
    * Update whitespace
    
    Co-authored-by: sjmiller609 <sjmiller609@gmail.com>
    Co-authored-by: Greg Neiheisel <greg@astronomer.io>

---
+++ prisma/include/wait-for-db.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+set -e
+
+wait_for_it() {
+    echo "Waiting for host: $1 $2"
+    while ! nc -w 1 -z "$1" "$2"; do
+        sleep 1
+    done
+    echo "Received response from: $1 $2"
+}
+
+# Parse string like: postgres://postgres:postgres@astronomer-postgresql.astronomer.svc.cluster.local:5432/astronomer_houston
+POSTGRES_HOST=$(echo "${PRISMA_DB_URI}" | awk -F@ '{print $2}' | awk -F: '{print $1}')
+POSTGRES_PORT=$(echo "${PRISMA_DB_URI}" | awk -F@ '{print $2}' | awk -F: '{print $2}' | awk -F/ '{print $1}')
+
+if [[ -n ${POSTGRES_HOST}  ]]; then
+    wait_for_it "$POSTGRES_HOST" "$POSTGRES_PORT"
+fi
+
+# Run the original command
+exec "$@"
