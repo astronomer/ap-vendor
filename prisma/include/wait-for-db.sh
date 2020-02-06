73ef3d73ed8a2cec987faa9f2e47bc2f8639af3e
schnie <greg@astronomer.io>
2020-02-06 15:53:32 -0500

    Update whitespace

--- prisma/include/wait-for-db.sh
+++ prisma/include/wait-for-db.sh
@@ -10,12 +10,13 @@ wait_for_it() {
     echo "Received response from: $1 $2"
 }
 
-# parse string like: postgres://postgres:postgres@astronomer-postgresql.astronomer.svc.cluster.local:5432/astronomer_houston
+# Parse string like: postgres://postgres:postgres@astronomer-postgresql.astronomer.svc.cluster.local:5432/astronomer_houston
 POSTGRES_HOST=$(echo "${PRISMA_DB_URI}" | awk -F@ '{print $2}' | awk -F: '{print $1}')
 POSTGRES_PORT=$(echo "${PRISMA_DB_URI}" | awk -F@ '{print $2}' | awk -F: '{print $2}' | awk -F/ '{print $1}')
 
 if [[ -n ${POSTGRES_HOST}  ]]; then
     wait_for_it "$POSTGRES_HOST" "$POSTGRES_PORT"
 fi
+
 # Run the original command
 exec "$@"
