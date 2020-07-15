4b6df00cd2b9bed0932c9a6f4ed26f6f0bd6280c
Steven Miller <sjmiller609@gmail.com>
2020-07-15 16:12:13 -0400

    Improve statsd metrics cardinality (#37)
    * Improve statsd metrics cardinality
    
    * Update ES version to see if that fixes security warnings
    
    * Set up testing framework
    
    * Let's not do the ES patching in this PR
    
    * Add tests
    
    * Remove unnecessary gitignore

---
+++ statsd-exporter/test/README.md
@@ -0,0 +1,34 @@
+# Testing Statsd-exporter
+
+This test scaffolding will bring up statsd-exporter and statsd with docker compose, hooking them up together. The component testing sends metrics to statsd to act like Airflow.
+
+## Play around with it
+
+In this directory, you can run
+
+```
+docker-compose up
+```
+
+Then, you can view the available metrics http://localhost:9102
+
+You can add a new metric to statsd
+
+```
+echo "airflow.operator_successes_PythonOperator:1|c" | nc -u -w0 127.0.0.1 8125
+```
+
+Then refresh your browser, you can find a metric:
+```
+airflow_operator_successes 1
+```
+
+## Run the component testing
+
+This basically will do the same thing as above in the setup py test fixture, then tear it down after it's done running the tests. The tests send metrics to statsd, then make assertions about the behavior of statsd-exporter. This serves well to isolate the behavior of statsd-exporter and its configuration without involving Kubernetes, Airflow, and so forth.
+
+```
+/bin/bash setup-test-env.sh
+source ./venv/bin/activate
+pytest ./test.py
+```
---
+++ statsd-exporter/test/test.py
@@ -0,0 +1,73 @@
+# content of test_module.py
+import pytest
+import requests
+import socket
+import subprocess
+from time import sleep
+from prometheus_client.parser import text_string_to_metric_families
+from statsd import StatsClient
+
+def _increment_metric(statsd_metric):
+    """
+    Send messages to statsd, this is similar to:
+    echo "airflow.operator_successes_PythonOperator:1|c" | nc -u -w0 127.0.0.1 8125
+    """
+    statsd = StatsClient(host='127.0.0.1',
+                         port=8125,
+                         prefix='airflow')
+    statsd.incr(statsd_metric)
+    # Avoid race conditions in our testing. After sending the data to
+    # statsd, we should allow time for statsd exporter to collect
+    # and serve new values
+    sleep(0.5)
+
+class Metric():
+    def __str__(self):
+        return f"name: {self.name}, labels: {self.labels}, value: {self.value}"
+
+    def __init__(self, name, labels={}, value=None):
+        self.name = name
+        self.labels = labels
+        self.value = value
+
+def _get_metrics():
+    response = requests.get("http://localhost:9102/metrics")
+    print(response.text)
+    for family in text_string_to_metric_families(response.text):
+        for sample in family.samples:
+            yield Metric(sample[0], labels=sample[1], value=sample[2])
+
+def _get_metric_by_name(name):
+    found_metrics = []
+    for metric in _get_metrics():
+        if metric.name == name:
+            return metric
+        found_metrics.append(metric.name)
+    raise Exception(f"Did not find metric {name}, only found metrics: {found_metrics}")
+
+def test_server_running():
+    response = requests.get("http://localhost:9102")
+    assert response.status_code == 200
+
+def test_increment_metric():
+    _increment_metric("scheduler_heartbeat")
+    metric = _get_metric_by_name("airflow_scheduler_heartbeat_total")
+    assert metric.value == 1
+    _increment_metric("scheduler_heartbeat")
+    metric = _get_metric_by_name("airflow_scheduler_heartbeat_total")
+    assert metric.value == 2
+
+def test_operators_conflated_to_single_metric():
+    _increment_metric("operator_successes_PythonOperator")
+    metric = _get_metric_by_name("airflow_operator_successes_total")
+    assert metric.value == 1
+    _increment_metric("operator_successes_BashOperator")
+    metric = _get_metric_by_name("airflow_operator_successes_total")
+    assert metric.value == 2
+
+@pytest.fixture(autouse=True, scope='session')
+def statsd_docker_compose():
+    subprocess.run("docker-compose up --always-recreate-deps --force-recreate --build -d", shell=True)
+    sleep(1)
+    yield
+    subprocess.run("docker-compose down", shell=True)
