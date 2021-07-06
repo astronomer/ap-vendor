3fb4c882512341a56db7169da5275fce8f8661f7
Daniel Hoherd <daniel.hoherd@gmail.com>
2021-07-06 14:27:06 -0700

    Try trivy-scan.sh

---
+++ bin/trivy-scan.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+[ "$#" == 1 ] || { echo "ERROR: Must give exactly one thing to scan, in the form of an ap-vendor directory." ; exit 1 ; }
+[ -f /etc/os-release ] && cat /etc/os-release
+
+GIT_ROOT="$(git -C "${0%/*}" rev-parse --show-toplevel)"
+scan_target="$1"
+
+set +exo pipefail
+
+trivy \
+  --ignorefile "${GIT_ROOT}/ap-${scan_target}/trivyignore" \
+  --ignorefile "${GIT_ROOT}/${scan_target}/trivyignore" \
+  --cache-dir /tmp/workspace/trivy-cache \
+  --ignore-unfixed -s HIGH,CRITICAL \
+  --exit-code 1 \
+  --no-progress \
+  "ap-${scan_target}" > "${GIT_ROOT}/trivy-output.txt"
+exit_code=$?
+
+cat "${GIT_ROOT}/trivy-output.txt"
+
+# Trivy cannot detect vulnerabilities not installed by package managers (EG: busybox, buildroot, make install):
+# - https://github.com/aquasecurity/trivy/issues/481 2020-04-30
+if grep -q -i 'OS is not detected' trivy-output.txt ; then
+  echo "Skipping trivy scan because of unsupported OS"
+  exit 0
+fi
+
+exit "${exit_code}"
