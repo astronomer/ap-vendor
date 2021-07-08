58533af9e91260ffc363c990b70876fc00ecca9c
Daniel Hoherd <daniel.hoherd@gmail.com>
2021-07-08 13:36:25 -0700

    Add label: quay.expires-after

--- bin/docker-build.sh
+++ bin/docker-build.sh
@@ -3,6 +3,22 @@ set -xe
 
 [[ $# -eq 2 ]] || exit 1
 
+labels=(
+    "--label=io.astronomer.build.branch=$CIRCLE_BRANCH"
+    "--label=io.astronomer.build.date=$(date +%F)"
+    "--label=io.astronomer.build.job.id=$CIRCLE_BUILD_NUM"
+    "--label=io.astronomer.build.job.name=$CIRCLE_JOB"
+    "--label=io.astronomer.build.repo=$CIRCLE_REPOSITORY_URL"
+    "--label=io.astronomer.build.sha=$CIRCLE_SHA1"
+    "--label=io.astronomer.build.unixtime=$(date +%s)"
+    "--label=io.astronomer.build.url=$CIRCLE_BUILD_URL"
+    "--label=io.astronomer.build.workflow.id=$CIRCLE_WORKFLOW_ID"
+)
+
+if ! [[ "$CIRCLE_BRANCH" =~ ^(master|main)$ ]] ; then
+    labels+=( "--label quay.expires-after=8w" )
+fi
+
 project_path="$1"
 image_name="$2"
 docker build \
@@ -10,16 +26,8 @@ docker build \
     --platform linux/amd64 \
     --tag "$image_name" \
     --file "$project_path"/Dockerfile \
-    --label io.astronomer.build.branch="$CIRCLE_BRANCH" \
-    --label io.astronomer.build.date="$(date +%F)" \
-    --label io.astronomer.build.job.id="$CIRCLE_BUILD_NUM" \
-    --label io.astronomer.build.job.name="$CIRCLE_JOB" \
-    --label io.astronomer.build.repo="$CIRCLE_REPOSITORY_URL" \
-    --label io.astronomer.build.sha="$CIRCLE_SHA1" \
-    --label io.astronomer.build.unixtime="$(date +%s)" \
-    --label io.astronomer.build.url="$CIRCLE_BUILD_URL" \
-    --label io.astronomer.build.workflow.id="$CIRCLE_WORKFLOW_ID" \
     --build-arg BUILD_NUMBER="$CIRCLE_BUILD_NUM" \
+    "${labels[@]}" \
     "$project_path"
 
 docker save -o "$image_name.tar" "$image_name"
