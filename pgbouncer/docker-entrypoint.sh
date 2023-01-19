d74ab355a06dd67ea8fca5d18533e948db00fbfa
Andrii Soldatenko <andrii.soldatenko@gmail.com>
2023-01-19 18:18:27 +0100

    refactor

--- pgbouncer/docker-entrypoint.sh
+++ pgbouncer/docker-entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 # Catch SIGTERM (immediate shutdown) and replace it with INT (safe shutdown)
 # so pgbouncer can signal postgres clients that it is shutting down.
+#
+# SIGTERM - Immediate shutdown for pgbouncer process,
+# but we would like to catch such signal and replace with SIGINT (Safe shutdown.)
 set -ex
 
 cleanup() {
@@ -10,9 +13,7 @@ cleanup() {
   wait "$main_proc"
   echo exited with "$?"
 }
-# SIGTERM - Immediate shutdown for pgbouncer process,
-# but we would like to catch such signal and replace with SIGINT (Safe shutdown.)
-trap "cleanup" TERM INT
+trap "cleanup" EXIT HUP INT QUIT PIPE TERM
 
 # Run the real command in background so we can still respond to signals.
 "$@" &
