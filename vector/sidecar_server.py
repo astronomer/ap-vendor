5a1416d180d465caae785c78cceb1619e611ce60
Daniel Hoherd <daniel.hoherd@gmail.com>
2023-04-12 15:47:08 -0400

    Remove HTTP signaling. Watch for 'finished' file as a signal from airflow that it is done.

--- vector/sidecar_server.py
+++ vector/sidecar_server.py
@@ -6,67 +6,63 @@ representing the last heartbeat from the airflow container.
 import time
 import os
 import subprocess
-from http import server
 from pathlib import Path
 
 ppid = os.getppid()
-
-proc = subprocess.Popen("/usr/local/bin/vector", shell=True)
-
-
-def quit_proc():
-    try:
-        print("Terminating.", flush=True)
-        proc.terminate()
-        proc.wait(timeout=60)
-    except subprocess.TimeoutExpired:
-        print("Termination timed out. Killing.", flush=True)
-        proc.kill()
-
-
-class MessageHandler(server.BaseHTTPRequestHandler):
-    """Listen for a POST to /quitquitquit to signal that the sidecar should exit. Also keep
-    an eye on the heartbeat file to see if the primary container has been brutally killed.
-    """
-
-    def do_POST(self):
-        if self.path != "/quitquitquit":
-            return
-
-        self.send_response(200)
-        self.end_headers()
-
-        quit_proc()
-        raise SystemExit()
-
-
-class MessageServer(server.HTTPServer):
-    heartbeat_file = Path("/var/log/sidecar-log-consumer/heartbeat")
-    heartbeat_max_age = 180  # seconds
-    last_timestamp = None
-
-    def service_actions(self):
-        proc.poll()
-        if proc.returncode is not None:
-            raise SystemExit(proc.returncode)
-
-        if self.heartbeat_file.exists():
-            # Sometimes the file contents are empty due to a race condition, so we only update
-            # last_timestamp if the file contents can be converted to a float.
-            if last_timestamp := float(self.heartbeat_file.read_text()):
-                self.last_timestamp = last_timestamp
-            age = time.time() - self.last_timestamp
-            if age > self.heartbeat_max_age / 2:
-                print(
-                    f"WARNING: Heartbeat has not been sent for {age:0.1f} seconds",
-                    flush=True,
-                )
-            if age > self.heartbeat_max_age:
-                raise SystemExit("ERROR: Heartbeat is gone. Exiting.")
-
-
 print(f"{ppid=}", flush=True)
 
-address = ("127.0.0.1", 8000)
-server = MessageServer(address, MessageHandler)
-server.serve_forever()
+
+class VectorHandler:
+    airflow_finished_file = Path("/var/log/sidecar-log-consumer/finished")
+    airflow_heartbeat_file = Path("/var/log/sidecar-log-consumer/heartbeat")
+    airflow_heartbeat_max_age = 120  # seconds
+    airflow_heartbeat_timestamp = None
+    vector = subprocess.Popen("ping -c 999 google.com", shell=True)
+
+    def quit_proc(self):
+        """Ask vector to quit nicely, and kill it after 60 if it does not quit."""
+        try:
+            print("Terminating.", flush=True)
+            self.vector.terminate()
+            self.vector.wait(timeout=60)
+        except subprocess.TimeoutExpired:
+            print("Termination timed out. Killing.", flush=True)
+            self.vector.kill()
+
+    def check_heartbeat(self):
+        if self.airflow_heartbeat_file.exists():
+            # Sometimes the file contents are empty due to a race condition, so we only update
+            # airflow_heartbeat_timestamp if the file contents can be converted to a float.
+            if airflow_heartbeat_timestamp := float(
+                self.airflow_heartbeat_file.read_text()
+            ):
+                self.airflow_heartbeat_timestamp = airflow_heartbeat_timestamp
+
+        if self.airflow_heartbeat_timestamp:
+            self.airflow_heartbeat_age = time.time() - self.airflow_heartbeat_timestamp
+            if self.airflow_heartbeat_age > self.airflow_heartbeat_max_age / 2:
+                print(
+                    f"WARNING: Heartbeat has not been sent for {self.airflow_heartbeat_age:0.1f} seconds",
+                    flush=True,
+                )
+            if self.airflow_heartbeat_age > self.airflow_heartbeat_max_age:
+                raise SystemExit("ERROR: Heartbeat is gone. Exiting.")
+
+    def run(self):
+        """Run a loop that handles signals from the Airflow container and monitors the proc. Exit when needed."""
+        while not self.airflow_finished_file.exists():
+            self.vector.poll()
+            if self.vector.returncode is not None:
+                print("Vector has quit. Exiting.")
+                raise SystemExit(self.vector.returncode)
+            self.check_heartbeat()
+            time.sleep(5)
+
+        print("Airflow has exited.")
+
+
+handler = VectorHandler()
+try:
+    handler.run()
+finally:
+    handler.quit_proc()
