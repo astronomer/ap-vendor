fbe22c56994fe6671c58ec179c59a8b61b2e0ea1
Mishal Shah <100714644+mishah334@users.noreply.github.com>
2023-04-15 00:27:16 +0530

    Adding Custom Git Sync Relay Docker Image (#512)

---
+++ git-sync-relay/gitsync.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+set -e
+
+GIT_SSH_COMMAND="ssh -o PasswordAuthentication=no -o KbdInteractiveAuthentication=no -o KbdInteractiveDevices=no"
+
+if [ ! -d "$GIT_SYNC_ROOT" ]; then
+  echo "Error: GIT_SYNC_ROOT ${GIT_SYNC_ROOT} does not exist!"
+  exit 1
+fi
+
+if [ -n "$(ls -A "$GIT_SYNC_ROOT")" ]; then
+  echo "Error: GIT_SYNC_ROOT ${GIT_SYNC_ROOT} is not empty."
+  exit 1
+fi
+
+if [ -n "$GIT_SYNC_SSH" ] && [ "true" = "$GIT_SYNC_SSH" ]; then
+  GIT_SSH_COMMAND="$GIT_SSH_COMMAND -i $GIT_SSH_KEY_FILE"
+
+  if [ -n "$GIT_KNOWN_HOSTS" ] && [ "true" = "$GIT_KNOWN_HOSTS" ]; then
+    GIT_SSH_COMMAND="$GIT_SSH_COMMAND -o StrictHostKeyChecking=yes -o UserKnownHostsFile=$GIT_SSH_KNOWN_HOSTS_FILE"
+  else
+    GIT_SSH_COMMAND="$GIT_SSH_COMMAND -o StrictHostKeyChecking=yes"
+  fi
+
+  export GIT_SSH_COMMAND
+fi
+
+cd "$GIT_SYNC_ROOT"
+
+# Clone the repo for the first time
+if [ "$GIT_SYNC_DEPTH" -gt 0 ]; then
+  git clone --depth "$GIT_SYNC_DEPTH" --branch "$GIT_SYNC_BRANCH" "$GIT_SYNC_REPO" .
+else
+  git clone --branch "$GIT_SYNC_BRANCH" "$GIT_SYNC_REPO" .
+fi
+
+while true; do
+  git fetch origin
+  git reset --hard "origin/$GIT_SYNC_BRANCH"
+  sleep "$GIT_SYNC_WAIT"
+done
