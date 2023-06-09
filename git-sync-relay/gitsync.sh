6c5d8d3f895421498a1e4a0d2e9366cc9e997411
pgvishnuram <vishnu@astronomer.io>
2023-06-09 17:48:45 +0530

    update clone logic

--- git-sync-relay/gitsync.sh
+++ git-sync-relay/gitsync.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -ex
+set -e
 
 GIT_SSH_COMMAND="ssh -o PasswordAuthentication=no -o KbdInteractiveAuthentication=no -o KbdInteractiveDevices=no"
 
@@ -36,14 +36,14 @@ fi
 cd "$GIT_SYNC_ROOT"
 
 # Clone the repo for the first time
-if grep -qF "$GIT_SYNC_REPO" "$GIT_SYNC_ROOT/.git/config"; then
-  echo "REPO already exists. Skipping clone"
-else
+if [ ! -f "$GIT_SYNC_ROOT/.git/config" ]; then
   if [ "$GIT_SYNC_DEPTH" -gt 0 ]; then
     git clone --depth "$GIT_SYNC_DEPTH" --branch "$GIT_SYNC_BRANCH" "$GIT_SYNC_REPO" .
   else
     git clone --branch "$GIT_SYNC_BRANCH" "$GIT_SYNC_REPO" .
   fi
+else
+  echo "REPO already exists. Skipping clone"
 fi
 
 while true; do
