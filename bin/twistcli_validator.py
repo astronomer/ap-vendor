0fc03ddcd7c78e16ffbf6b7a4628cd0d84f18fb5
pgvishnuram <81585115+pgvishnuram@users.noreply.github.com>
2023-09-26 23:57:38 +0530

    Update bin/twistcli_validator.py
    merging this change
    
    Co-authored-by: Daniel Hoherd <daniel.hoherd@gmail.com>

--- bin/twistcli_validator.py
+++ bin/twistcli_validator.py
@@ -1,34 +1,43 @@
 #!/usr/bin/env python3
 import json
-import sys
-import os
 from pathlib import Path
+import os
 
 
-ROOT_DIRECTORY = [x for x in Path(__file__).resolve().parents if (x / ".git").is_dir()][
+git_root = [x for x in Path(__file__).resolve().parents if (x / ".git").is_dir()][
     -1
 ]
-PROJECT_NAME = os.getenv("PROJECT_DIRECTORY")
-SCAN_RESULTS_FILE_PATH = f"{ROOT_DIRECTORY}/{PROJECT_NAME}"
-cve_list = []
+project_name = os.getenv("PROJECT_DIRECTORY")
+scan_results_file_path = f"{git_root}/{project_name}"
 
-data = json.loads(Path(f"{SCAN_RESULTS_FILE_PATH}/scan-results.json").read_text())
-cve_list = [
+data = json.loads(Path(f"{scan_results_file_path}/scan-results.json").read_text())
+found_cves = [
     vuln["id"] for vuln in data.get("results", [])[0].get("vulnerabilities", [])
 ]
 
-ignored_cve_list = []
-try:
-    with open(f"{SCAN_RESULTS_FILE_PATH}/twistcliignore", "r") as f:
-        ignored_cve_list = [line.strip() for line in f.read().splitlines()]
-        if ignored_cve_list == cve_list:
-            print("vulnerabilities is in ignored cve list and are tracked until fixed")
-        else:
-            print("New vulnerabilities found.Fix them or add into cve ignore list")
-            print([cve for cve in cve_list if cve not in ignored_cve_list])
-            sys.exit(1)
-except FileNotFoundError:
-    if cve_list:
-        print("New vulnerabilities found.Fix them or add into ignore list")
-        print([cve for cve in cve_list if cve not in ignored_cve_list])
-        sys.exit(1)
+def cve_list_to_string(list):
+    if list:
+        return '\n' + '\n'.join([f'  - {item}' for item in list]) + '\n'
+    return 'none\n'
+
+def get_ignored_cves():
+    ignore_file = Path(f"{scan_results_file_path}/twistcliignore")
+    if ignore_file.is_file():
+        return [line.strip() for line in ignore_file.read_text().splitlines()]
+    return []
+
+ignored_cve_list = get_ignored_cves()
+
+if found_cves:
+    print(f"New vulnerabilities found. Fix them or add them to {project_name}/twistcliignore\n")
+
+    new_cves = [cve for cve in found_cves if cve not in ignored_cve_list]
+    print(f"New CVEs that are not found in {project_name}/twistcliignore: {cve_list_to_string(new_cves)}")
+
+    old_cves = [cve for cve in found_cves if cve in ignored_cve_list and cve in found_cves]
+    print(f"Old CVEs that exist now but are already in {project_name}/twistcliignore: {cve_list_to_string(old_cves)}")
+
+    solved_cves = [cve for cve in ignored_cve_list if cve not in found_cves]
+    print(f"Solved CVEs that can be removed from {project_name}/twistcliignore: {cve_list_to_string(solved_cves)}")
+
+    raise SystemExit(1)
