4249638134ffe70395dc4328539fa6a4d87f2d0c
pgvishnuram <vishnu@astronomer.io>
2023-09-26 17:09:00 +0530

    add twistcli validator

---
+++ bin/twistcli_validator.py
@@ -0,0 +1,27 @@
+import json
+import sys
+import os
+from pathlib import Path
+
+
+ROOT_DIRECTORY = Path(__file__).parent.parent
+PROJECT_NAME = os.getenv("PROJECT_DIRECTORY")
+SCAN_RESULTS_FILE_PATH = f"{ROOT_DIRECTORY}/{PROJECT_NAME}"
+output = []
+
+with open(f"{SCAN_RESULTS_FILE_PATH}/scan-results.json", "r") as f:
+    data = json.load(f)
+    vulnerabilities = data.get("results", [])[0].get("vulnerabilities", [])
+    output = [vul["id"] for vul in vulnerabilities]
+
+try:
+    with open(f"{SCAN_RESULTS_FILE_PATH}/twistcliignore", "r") as f:
+        ignore_list = [line.strip() for line in f.read().splitlines()]
+        print(ignore_list)
+        if ignore_list == output:
+            print("vuln is in ignore list")
+        else:
+            print("New vulnerabilities found.Fix them or add into ignore list")
+            sys.exit(1)
+except FileNotFoundError:
+    pass
