5bd5694fa843e4a949795f20b55396d70152d779
pgvishnuram <vishnu@astronomer.io>
2023-09-26 19:22:43 +0530

    update twistcli validator

--- bin/twistcli_validator.py
+++ bin/twistcli_validator.py
@@ -10,18 +10,21 @@ ROOT_DIRECTORY = [x for x in Path(__file__).resolve().parents if (x / ".git").is
 ]
 PROJECT_NAME = os.getenv("PROJECT_DIRECTORY")
 SCAN_RESULTS_FILE_PATH = f"{ROOT_DIRECTORY}/{PROJECT_NAME}"
-output = []
+cve_list = []
 
 data = json.loads(Path(f"{SCAN_RESULTS_FILE_PATH}/scan-results.json").read_text())
-output = [vuln["id"] for vuln in data.get("results", [])[0].get("vulnerabilities", [])]
+cve_list = [
+    vuln["id"] for vuln in data.get("results", [])[0].get("vulnerabilities", [])
+]
 
 try:
     with open(f"{SCAN_RESULTS_FILE_PATH}/twistcliignore", "r") as f:
-        ignore_list = [line.strip() for line in f.read().splitlines()]
-        if ignore_list == output:
-            print("vuln is in ignore list")
+        ignored_cve_list = [line.strip() for line in f.read().splitlines()]
+        if ignored_cve_list == cve_list:
+            print("vulnerabilities is in ignored cve list and are tracked until fixed")
         else:
-            print("New vulnerabilities found.Fix them or add into ignore list")
+            print("New vulnerabilities found.Fix them or add into cve ignore list")
+            print([cve for cve in cve_list if cve not in ignored_cve_list])
             sys.exit(1)
 except FileNotFoundError:
     pass
