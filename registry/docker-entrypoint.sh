db07174f1bfdef3e2bd5656cfcb43822306ae3eb
pgvishnuram <vishnu@astronomer.io>
2023-12-26 12:19:49 +0530

    update version

--- registry/docker-entrypoint.sh
+++ registry/docker-entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 # In production, we mount the registry TLS certificate
 # into /usr/local/share/ca-certificates. This completes
 # the certificate installation so we can trust the registry.
-if [ "$UPDATE_CA_CERTS" == "true" ]; then
+if [ "$UPDATE_CA_CERTS" = "true" ]; then
 echo "Running update-ca-certificates"
 update-ca-certificates
 fi
