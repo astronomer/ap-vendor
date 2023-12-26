eee875afc13632ac13c498b66ecd068c523ffcf4
pgvishnuram <vishnu@astronomer.io>
2023-12-26 12:10:29 +0530

    add conditional config for update ca-certificates

--- registry/docker-entrypoint.sh
+++ registry/docker-entrypoint.sh
@@ -2,6 +2,14 @@
 
 set -e
 
+# In production, we mount the registry TLS certificate
+# into /usr/local/share/ca-certificates. This completes
+# the certificate installation so we can trust the registry.
+if [ "$UPDATE_CA_CERTS" == "true" ]; then
+echo "Running update-ca-certificates"
+update-ca-certificates
+fi
+
 case "$1" in
     *.yaml|*.yml) set -- registry serve "$@" ;;
     serve|garbage-collect|help|-*) set -- registry "$@" ;;
