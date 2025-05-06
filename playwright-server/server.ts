24451a5c3761a39150bb5c734991a8a14115267e
Stuart Buckingham <stu@astronomer.io>
2025-05-05 20:02:03 -0500

    Add playwright-server config

---
+++ playwright-server/server.ts
@@ -0,0 +1,14 @@
+const { chromium } = require('playwright');
+
+(async () => {
+  const browserServer = await chromium.launchServer({
+    port: 9222,
+    wsPath: "ws",
+    logger: {
+        isEnabled: (name, severity) => true,
+        log: (name, severity, message, args) => console.log(`${name} ${message}`)
+      }
+    });
+  const wsEndpoint = browserServer.wsEndpoint();
+  console.log("Listening on: ", wsEndpoint);
+})();
