c1b8a4a1799cb2f1e3b84304e2389d9d4ec3fe9a
Shubham-astro <shubham.singh@astronomer.io>
2026-09-02 13:57:27 +0530

    add trust

--- postgresql/docker-entrypoint-wrapper.sh
+++ postgresql/docker-entrypoint-wrapper.sh
@@ -31,7 +31,7 @@ if [ -f "$data_dir/PG_VERSION" ] && { [ ! -f "$data_dir/postgresql.conf" ] || [ 
   fi
 
   if [ -n "$scratch_dir" ]; then
-    if initdb_output="$(initdb --auth-local=scram-sha-256 --auth-host=scram-sha-256 --username=postgres --no-sync "$scratch_dir" 2>&1)"; then
+    if initdb_output="$(initdb --auth-local=trust --auth-host=trust --username=postgres --no-sync "$scratch_dir" 2>&1)"; then
       initdb_status=0
     else
       initdb_status=$?
